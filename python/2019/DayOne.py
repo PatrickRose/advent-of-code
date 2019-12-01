a466f631433e06c032dc7b6af499b3ab30a41619
Patrick Rose <pjr0911025@googlemail.com>
2019-12-01 23:26:30 +0000

    2019, day one

---
+++ python/2019/DayOne.py
@@ -0,0 +1,25 @@
+import sys, math
+
+def calculate_fuel(value):
+    return math.floor(value / 3) - 2
+
+input = sys.stdin.readlines()
+values = [];
+
+for line in input:
+    values.append(calculate_fuel(int(line.strip())))
+
+print ("Part 1: " + str(sum(values)))
+
+fuels = [];
+
+def calculate_module_fuel(value):
+    fuel_for_value = calculate_fuel(value)
+
+    if fuel_for_value <= 0:
+        return 0
+    
+    return fuel_for_value + calculate_module_fuel(fuel_for_value)
+
+print ("Part 2: " + str(sum(map(lambda x: x + calculate_module_fuel(x), values))))
+    

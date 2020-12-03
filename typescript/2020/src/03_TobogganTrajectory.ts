ab69ac7c3a5ed5b31491c4ae3000c7cfee5ed206
Patrick Rose <pjr0911025@googlemail.com>
2020-12-03 09:00:45 +0000

    Solve day 3

---
+++ typescript/2020/src/03_TobogganTrajectory.ts
@@ -0,0 +1,37 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync(__dirname + '/../input/day03.txt').toString('utf-8').trim();
+
+const trees: Array<Array<boolean>> = input.split("\n").map(
+    (row: string): Array<boolean> => {
+        return row.split('').map((char) => char == '#')
+    }
+);
+const maxRight = trees[0].length;
+
+function getPositionsInAngle(right: number, down: number): Array<{ x: number, y: number, hit: boolean }> {
+    let x = 0;
+
+    let treesHit = [];
+
+    for (let y = 0; y < trees.length; y += down) {
+        treesHit.push({
+            x, y, hit: trees[y][x]
+        });
+        x = (x + right) % maxRight;
+    }
+    return treesHit;
+}
+
+function countTreesInAngle(right: number, down: number = 1): number {
+    return getPositionsInAngle(right, down).filter(val => val.hit).length;
+}
+
+const r1r1 = countTreesInAngle(1);
+const r3d1 = countTreesInAngle(3);
+const r5d1 = countTreesInAngle(5);
+const r7d1 = countTreesInAngle(7);
+const r1d2 = countTreesInAngle(1, 2);
+
+console.log(`Part 1: ${r3d1}`);
+console.log(`Part 2: ${r1r1} * ${r3d1} * ${r5d1} * ${r7d1} * ${r1d2} = ${r1r1 * r3d1 * r5d1 * r7d1 * r1d2}`);

f3391744cd4a740573d4fd7f8f8dd7a66861af10
Patrick Rose <pjr0911025@googlemail.com>
2021-12-06 08:58:59 +0000

    Solve 2021 day 6

---
+++ typescript/2021/src/06_Lanternfish.ts
@@ -0,0 +1,55 @@
+import getInput from "./util/getInput";
+
+const exampleInput = `3,4,3,1,2`;
+
+const realInput = getInput(6);
+
+const input = realInput;
+
+const cacheMap = new Map<number, Map<number, number>>();
+
+class Fish {
+    constructor(public days: number) {
+    }
+
+    public tick(): boolean {
+        this.days--;
+
+        if (this.days < 0) {
+            this.days += 7;
+            return true;
+        }
+
+        return false;
+    }
+
+    public fishesMadeInPeriod(days: number): number {
+        let daysMap = cacheMap.get(days);
+        if (!daysMap) {
+            daysMap = new Map<number, number>();
+            cacheMap.set(days, daysMap);
+        }
+
+        const thisMap = daysMap.get(this.days);
+        if (thisMap) {
+            return thisMap;
+        }
+
+        let currentTime = this.days;
+        let fishesMade = 1;
+
+        while (currentTime < days) {
+            fishesMade += (new Fish(currentTime + 9).fishesMadeInPeriod(days));
+            currentTime += 7;
+        }
+
+        daysMap.set(this.days, fishesMade);
+
+        return fishesMade;
+    }
+}
+
+const fishes = input.split(',').map(val => new Fish(parseInt(val, 10)));
+
+console.log(`Part 1: ${fishes.map(fish => fish.fishesMadeInPeriod(80)).reduce((prev, curr) => prev + curr, 0)}`);
+console.log(`Part 2: ${fishes.map(fish => fish.fishesMadeInPeriod(256)).reduce((prev, curr) => prev + curr, 0)}`);

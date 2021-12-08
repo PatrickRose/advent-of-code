b6812d9a9e4591261122e3370c5dd87e2c6e9a3b
Patrick Rose <pjr0911025@googlemail.com>
2021-12-08 08:25:31 +0000

    Solve day 8 part 1

---
+++ typescript/2021/src/08_SevenSegmentSearch.ts
@@ -0,0 +1,23 @@
+import getInput from "./util/getInput";
+
+const exampleInput = `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`;
+
+const realInput = getInput(8);
+
+const input = realInput;
+
+const part1 = input.split("\n").map(val => val.split(' | ')[1].split(' ').filter(signal => [2, 4, 3, 7].includes(signal.length)).length).reduce((curr, val) => curr + val);
+
+console.log(part1);
+
+const actuals = new Map<string, string>();
+const possibles = new Map<string, string[]>();

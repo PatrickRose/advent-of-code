1afb31c42400d656c1ad9a8b325e58b23b19151d
Patrick Rose <pjr0911025@googlemail.com>
2021-12-18 23:24:39 +0000

    Add helper to download input

---
+++ typescript/src/downloadInput.ts
@@ -0,0 +1,61 @@
+import * as fs from "fs";
+import { getInputDirectory } from './util/getInput';
+import fetch from "node-fetch";
+import * as dotenv from "dotenv";
+import path = require("path");
+
+const inputDir = getInputDirectory();
+
+dotenv.config();
+
+if (!process.env.SESSION_COOKIE) {
+    console.log('No value for SESSION_COOKIE in env vars!');
+    console.log('You probably want to copy and edit .env.example')
+    process.exit(1);
+}
+
+let [year, day, force] = process.argv.slice(2);
+
+if (year === undefined) {
+    year = `${(new Date()).getFullYear()}`;
+}
+
+if (day === undefined) {
+    // Get the current day
+    day = `${Math.min((new Date).getDate(), 25)}`;
+}
+
+const fileName = `${inputDir}/${year}/day${day.length < 2 ? '0' : ''}${day}.txt`;
+
+// First, have we got the file? If so, tell the user that and do nothing
+if (!force && fs.existsSync(fileName)) {
+    console.log('Already got input!');
+    process.exit(0);
+}
+
+console.log('Fetching input from AoC...');
+
+fetch(
+    `https://adventofcode.com/${year}/day/${day}/input`,
+    {
+        headers: {
+            cookie: `session=${process.env.SESSION_COOKIE}`
+        }
+    }
+).then(response => {
+    if (!response.ok) {
+        return response.text().then(body => { throw new Error(body) });
+    }
+    return response.text();
+}).then(body => {
+    try {
+        fs.mkdirSync(path.dirname(fileName));
+    }
+    catch (e) {
+        // ignore the error
+    }
+    fs.writeFileSync(fileName, body)
+    console.log(`Written to ${fileName}`);
+}).catch(val => {
+    console.log(val);
+});
--- typescript/src/util/getInput.ts
+++ typescript/src/util/getInput.ts
@@ -1,14 +1,20 @@
 import * as fs from "fs";
 import * as path from "path";
 
-export default function getInput(year: number, day: number) {
-    const filename = day < 10 ? `0${day}` : day.toString(10)
-
+export function getInputDirectory(): string {
     let dir = fs.opendirSync(`${__dirname}`);
 
     while (path.basename(dir.path) != 'typescript') {
         dir = fs.opendirSync(path.dirname(dir.path));
     }
 
-    return fs.readFileSync(`${dir.path}/../input/${year}/day${filename}.txt`).toString('utf-8').trim();
+    return fs.opendirSync(`${path.dirname(dir.path)}/input`).path;
+}
+
+export default function getInput(year: number, day: number) {
+    const filename = day < 10 ? `0${day}` : day.toString(10);
+
+    const inputDir = getInputDirectory();
+
+    return fs.readFileSync(`${inputDir}/${year}/day${filename}.txt`).toString('utf-8').trim();
 }

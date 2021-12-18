6c3511501ef91d1c04dc51f7314617cf501d523f
Patrick Rose <pjr0911025@googlemail.com>
2021-12-18 08:40:07 +0000

    Use general input directory for file parsing

--- typescript/util/getInput.ts
+++ typescript/util/getInput.ts
@@ -3,5 +3,5 @@ import * as fs from "fs";
 export default function getInput(year: number, day: number) {
     const filename = day < 10 ? `0${day}` : day.toString(10)
 
-    return fs.readFileSync(`${__dirname}/../${year}/input/day${filename}.txt`).toString('utf-8').trim();
+    return fs.readFileSync(`${__dirname}/../../input/${year}/day${filename}.txt`).toString('utf-8').trim();
 }

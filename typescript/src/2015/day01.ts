8ea7d7f8800bb31e51f604a94c9182b86156081a
Patrick Rose <patrick.rose@theskillsnetwork.com>
2023-12-03 22:50:27 +0000

    Solve 2015 day 1

--- typescript/src/2015/day01.ts
+++ typescript/src/2015/day01.ts
@@ -3,3 +3,23 @@ import getInput from "./util/getInput";
 const testInputs = {}
 
 const input = getInput(testInputs, 1);
+
+const instructions: number[] = input.split('')
+    .map(val => val == '(' ? 1 : -1);
+
+const part1: number = instructions
+    .reduce((previousValue, currentValue) => previousValue+currentValue);
+
+console.log(`Part 1: ${part1}`)
+
+let pos = 0;
+instructions.forEach((val, index) => {
+    if (pos == -1) {
+        return;
+    }
+    pos += val;
+
+    if (pos == -1) {
+        console.log(`Part 2: ${index + 1}`)
+    }
+})

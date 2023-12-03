56caf906eb6fc0e9e888af74572a22e3aa1a5a27
Patrick Rose <patrick.rose@theskillsnetwork.com>
2023-12-03 23:01:40 +0000

    Solve 2015 day 2

--- typescript/src/2015/day02.ts
+++ typescript/src/2015/day02.ts
@@ -1,5 +1,45 @@
 import getInput from "./util/getInput";
+import parseInt from "../util/parseInt";
 
 const testInputs = {}
 
 const input = getInput(testInputs, 2);
+
+const presents: { l: number, w: number, h: number }[] = input.split('\n').map(
+    row => {
+        const [l, w, h] = row.split('x');
+
+        return {l: parseInt(l), w: parseInt(w), h: parseInt(h)}
+    }
+);
+
+const part1 = presents.map(
+    ({l, w, h}) => {
+        const lw = l * w;
+        const wh = w * h;
+        const hl = h * l;
+
+        const toAdd = [lw, lw, wh, wh, hl, hl];
+
+        toAdd.sort((a, b) => a - b);
+        toAdd.push(toAdd[0]);
+
+        return toAdd.reduce((prev, curr) => prev + curr);
+    })
+    .reduce((prev, curr) => prev + curr);
+
+console.log(`Part 1: ${part1}`);
+
+const part2 = presents.map(
+    ({l, w, h}) => {
+        const toAdd = [l, w, h];
+
+        toAdd.sort((a, b) => a - b);
+        toAdd.pop();
+
+        return 2 * toAdd[0] + 2 * toAdd[1] + (l * w * h);
+    })
+    .reduce((prev, curr) => prev + curr);
+
+console.log(`Part 2: ${part2}`);
+

6826004e4c54bce328ad7887631989edfebbe739
Patrick Rose <patrick.rose@theskillsnetwork.com>
2023-12-05 12:29:45 +0000

    Update type of getInput to be more ergonomic
    We don't need two generics, it's all related to the key type (K)

--- typescript/src/2023/util/getInput.ts
+++ typescript/src/2023/util/getInput.ts
@@ -4,7 +4,7 @@ function getRealInput(day: number) {
     return getInputBase(2023, day)
 }
 
-export default function getInput<K extends string, T extends Record<K, string>>(testInputs: T, day: number, inputType: (K | true) = true): string {
+export default function getInput<K extends string>(testInputs: Record<K, string>, day: number, inputType: (K | true) = true): string {
     if (inputType === true) {
         return getRealInput(day);
     }

a50e3d4e39b4095b76e6e3ec52f5ed3cd6cd94f2
Patrick Rose <patrick.rose@theskillsnetwork.com>
2023-12-06 21:46:39 +0000

    Solve 2015 day 10

--- typescript/src/2015/day10.ts
+++ typescript/src/2015/day10.ts
@@ -1,5 +1,30 @@
 import getInput from "./util/getInput";
 
-const testInputs = {}
+const testInputs = {
+    example: '1'
+}
 
 const input = getInput(testInputs, 10);
+
+const numIterations = input == '1' ? 5 : 40;
+
+let answer = input;
+
+function lookAndSay(word: string): string {
+    return word.replaceAll(
+        /(.)\1*/g,
+        (match) => `${match.length}${match[0]}`
+    )
+}
+
+for (let i = 0; i < numIterations; i++) {
+    answer = lookAndSay(answer);
+}
+
+console.log(`Part 1: ${answer.length}`);
+
+for (let i = 0; i<10; i++) {
+    answer = lookAndSay(answer);
+}
+
+console.log(`Part 2: ${answer.length}`);

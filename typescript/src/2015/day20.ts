e68c07cb01aa4ce7e6bdde09e37e1e7236581b18
Patrick Rose <patrick.rose@theskillsnetwork.com>
2024-09-04 23:23:03 +0100

    Solve 2015 day 20

--- typescript/src/2015/day20.ts
+++ typescript/src/2015/day20.ts
@@ -3,3 +3,42 @@ import getInput from "./util/getInput";
 const testInputs = {}
 
 const input = getInput(testInputs, 20);
+
+const target = Number.parseInt(input, 10);
+
+const presents: number[] = [];
+const presents2: number[] = [];
+
+for(let e = 1; e < (target/10); e++) {
+    let visits = 0;
+    for(let i=e; i< (target/10); i += e) {
+        if (!presents[i]) {
+            presents[i] = 10;
+        }
+
+        presents[i] += (e*10);
+
+        if (visits < 50) {
+            if (!presents2[i]) {
+                presents2[i] = (e*11);
+            } else {
+                presents2[i] += (e*11);
+            }
+            visits++;
+        }
+    }
+}
+
+function findMinimum(houses: number[]): number {
+    let house = 1;
+
+    while (houses[house] < target) {
+        house++;
+    }
+
+    return house;
+}
+
+
+console.log(`Part 1: ${findMinimum(presents)}`);
+console.log(`Part 1: ${findMinimum(presents2)}`);

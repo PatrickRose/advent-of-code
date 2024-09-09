c6918913da2190b9ffd697edfd9cfcdc4fc26cc2
Patrick Rose <patrick.rose@theskillsnetwork.com>
2024-09-09 23:29:23 +0100

    Solve 2015 day 25

--- typescript/src/2015/day25.ts
+++ typescript/src/2015/day25.ts
@@ -3,3 +3,30 @@ import getInput from "./util/getInput";
 const testInputs = {}
 
 const input = getInput(testInputs, 25);
+
+const parts = input.match(/\D+(\d+)\D+(\d+)/);
+
+if (parts == null) {
+    throw new Error('Invalid input');
+}
+
+const y = Number.parseInt(parts[1], 10);
+const x = Number.parseInt(parts[2], 10);
+
+let val = 20151125;
+let xPos = 1;
+let yPos = 1;
+
+while (xPos != x || yPos != y) {
+    if (yPos == 1) {
+        yPos = xPos + 1;
+        xPos = 1;
+    } else {
+        yPos--;
+        xPos++;
+    }
+
+    val = (val * 252533) % 33554393;
+}
+
+console.log(`Part 1: ${val}`)

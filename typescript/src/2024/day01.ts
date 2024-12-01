983c9c89db853bb731863c6877c5eb5956c1e0a3
Patrick Rose <patrick.rose@theskillsnetwork.com>
2024-12-01 08:51:54 +0000

    Solve 2024 day 1

--- typescript/src/2024/day01.ts
+++ typescript/src/2024/day01.ts
@@ -1,5 +1,37 @@
 import getInput from "./util/getInput";
+import {accumulator} from "../util/accumulator";
 
-const testInputs = {}
+const testInputs = {
+    example: `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
+}
 
 const input = getInput(testInputs, 1);
+
+const leftList: number[] = [];
+const rightList: number[] = [];
+
+input.split('\n').forEach(row => {
+    const [left, right] = row.split(/\s+/).map(val => Number.parseInt(val, 10));
+    leftList.push(left);
+    rightList.push(right);
+})
+
+leftList.sort((a,b) => a-b);
+rightList.sort((a,b) => a-b);
+
+const diffs = leftList.map((_, index) => Math.abs(leftList[index] - rightList[index]));
+
+console.log(`Part 1: ${accumulator(diffs)}`)
+
+const similarities = leftList.map(val => {
+    const appears = rightList.filter(r => val == r).length;
+
+    return val * appears;
+})
+
+console.log(`Part 2: ${accumulator(similarities)}`);

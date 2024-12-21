b822392beb396955d2f1e0870f1d00ecd3e1d463
Patrick Rose <patrick.rose@theskillsnetwork.com>
2024-12-21 10:41:19 +0000

    Add a point is equivalent helper
    I keep needing to do this and its very annoying

--- typescript/src/util/points.ts
+++ typescript/src/util/points.ts
@@ -202,3 +202,7 @@ export function strToPoint3D(input: string): Point3D {
 
     return {x, y, z}
 }
+
+export function pointMatches(a: Point, b: Point): boolean {
+    return a.x == b.x && a.y == b.y;
+}

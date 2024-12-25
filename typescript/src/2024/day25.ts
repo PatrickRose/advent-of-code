aa66f73c1751946bc3f07f2cdbda744cf682ff9d
Patrick Rose <patrick.rose@theskillsnetwork.com>
2024-12-25 08:51:28 +0000

    Solve 2024 day 25

--- typescript/src/2024/day25.ts
+++ typescript/src/2024/day25.ts
@@ -1,5 +1,78 @@
 import getInput from "./util/getInput";
+import {mappedAccumulator} from "../util/accumulator";
 
-const testInputs = {}
+const testInputs = {
+    example: `#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####`
+}
 
 const input = getInput(testInputs, 25);
+
+type KeyOrLock = [number, number, number, number, number];
+
+const keys: KeyOrLock[] = [];
+const locks: KeyOrLock[] = [];
+
+input.split('\n\n').forEach((possible) => {
+    const rows = possible.split('\n');
+    let isKey = rows[0] == '.....';
+    let toReturn: KeyOrLock = [0,0,0,0,0];
+    for(const row of rows.slice(1)) {
+        for(let i=0; i<5;i++) {
+            if (row[i] == '#') {
+                toReturn[i]++;
+            }
+        }
+    }
+
+    if (isKey) {
+        keys.push(toReturn);
+    } else {
+        locks.push(toReturn);
+    }
+});
+
+const part1 = mappedAccumulator(locks, (lock) => {
+    return keys.filter((key) => key.every((val, index) => {
+        return val + lock[index] <= 6;
+    })).length
+});
+
+console.log(`Part 1: ${part1}`);

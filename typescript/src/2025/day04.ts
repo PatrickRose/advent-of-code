32b8889160bbeb86e4a66971492e8477c11a06c1
Patrick Rose <patrick.rose@theskillsnetwork.com>
2025-12-04 09:17:00 +0000

    Solve 2025 day 4

--- typescript/src/2025/day04.ts
+++ typescript/src/2025/day04.ts
@@ -1,5 +1,64 @@
 import getInput from "./util/getInput";
+import {forEachPoint, getAdjacentPoints, getPoint, Point, PointMap, setPoint, unsetPoint} from "../util/points";
 
-const testInputs = {}
+const testInputs = {
+    example: `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`
+}
 
 const input = getInput(testInputs, 4);
+
+const map: PointMap<boolean> = new Map<number, Map<number, true>>();
+
+input.split('\n').forEach((line, y) => {
+    line.split('').forEach((char, x) => {
+        if (char == '@') {
+            setPoint({x,y}, true, map);
+        }
+    })
+});
+
+let allowedPoints = 0;
+
+forEachPoint(map, ({x,y}, val) => {
+    const numAllowed = getAdjacentPoints(x,y).filter(point => getPoint(point, map)).length;
+
+    if (numAllowed < 4) {
+        allowedPoints++;
+    }
+})
+
+console.log(`Part 1: ${allowedPoints}`);
+
+let removed = 0;
+const points: Point[] = [];
+
+while (true) {
+    forEachPoint(map, ({x,y}, val) => {
+        const numAllowed = getAdjacentPoints(x,y).filter(point => getPoint(point, map)).length;
+
+        if (numAllowed < 4) {
+            points.push({x,y});
+        }
+    })
+
+    if (points.length == 0) {
+        break;
+    }
+
+    removed += points.length;
+    let point: Point|undefined;
+    while (point = points.pop()) {
+        unsetPoint(point, map)
+    }
+}
+
+console.log(`Part 2: ${removed}`);

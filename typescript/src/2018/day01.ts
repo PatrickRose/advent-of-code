28dbf04f7ba02afa1db6494380ffb8883f12e15d
Patrick Rose <patrick.rose@theskillsnetwork.com>
2025-12-29 11:39:53 +0000

    Solve 2018 day 1

--- typescript/src/2018/day01.ts
+++ typescript/src/2018/day01.ts
@@ -1,5 +1,36 @@
 import getInput from "./util/getInput";
+import {mappedAccumulator} from "../util/accumulator";
 
-const testInputs = {}
+const testInputs = {
+    example: `+1
+-2
++3
++1`
+}
 
 const input = getInput(testInputs, 1);
+
+const numbers = input.split('\n').map(val => Number.parseInt(val, 10));
+
+const part1 = mappedAccumulator(numbers, (val) => val);
+
+console.log(`Part 1: ${part1}`)
+
+const hits = new Set<number>();
+
+let currVal = 0;
+let i = 0;
+
+while (true) {
+    hits.add(currVal);
+    const val = numbers[i % numbers.length];
+
+    currVal += val;
+
+    if (hits.has(currVal)) {
+        console.log(`Part 2: ${currVal}`);
+        break;
+    }
+
+    i++;
+}

b0647ef01ca33634f7ef006e025e0ae8fb85d82b
Patrick Rose <patrick.rose@theskillsnetwork.com>
2026-01-02 22:42:12 +0000

    Solve 2018 day 5

--- typescript/src/2018/day05.ts
+++ typescript/src/2018/day05.ts
@@ -1,5 +1,43 @@
 import getInput from "./util/getInput";
 
-const testInputs = {}
+const testInputs = {
+    aA: 'aA',
+    abBA: 'abBA',
+    abAB: 'abAB',
+    aabAAB: 'aabAAB',
+    example: 'dabAcCaCBAcCcaDA'
+}
 
 const input = getInput(testInputs, 5);
+
+function performReaction(input: string): number {
+    let removed: boolean;
+    do {
+        removed = false;
+
+        for (let i = 0; i < 26; i++) {
+            for (const toReplace of [String.fromCharCode(65 + i, 65 + 32 + i), String.fromCharCode(65 + 32 + i, 65 + i)]) {
+                if (input.includes(toReplace)) {
+                    input = input.replaceAll(toReplace, '');
+                    removed = true;
+                }
+            }
+        }
+    } while (removed)
+
+    return input.length;
+}
+
+console.log(`Part 1: ${performReaction(input)}`);
+
+let min = Infinity;
+for (let i = 0; i < 26; i++) {
+    const upperCase = String.fromCharCode(65 + i);
+    const lowerCase = upperCase.toLowerCase();
+    const toTry = input.replaceAll(upperCase, '')
+        .replaceAll(lowerCase, '');
+
+    min = Math.min(min, performReaction(toTry));
+}
+
+console.log(`Part 2: ${min}`);
